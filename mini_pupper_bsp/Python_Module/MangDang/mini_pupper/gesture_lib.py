af03428c364c33fb5d7f2b4a7fa49e4bf108dedf
Yuxuan He <heyux@oregonstate.edu>
2024-06-28 12:52:17 -0700

    emm

--- mini_pupper_bsp/Python_Module/MangDang/mini_pupper/gesture_lib.py
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/gesture_lib.py
@@ -22,46 +22,54 @@ counter_a = 0
 
 class Gesture:
 
-    #gesture 1 stand
+    @staticmethod
     def default():
         global counter_a
 
-        #starting leg positions
-        initial_position = [512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512]  
+        print("Executing default gesture")
+        # starting leg positions
+        initial_position = [512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512]
         esp32.servos_set_position(initial_position)
 
-        msg,counter_a = helper_movement.toggle_activation(counter_a)
+        msg, counter_a = helper_movement.toggle_activation(counter_a)
         helper_movement.pub_msg(msg, wait_time)
+        print("Default gesture completed")
 
-    #gesture 2 sit
+    @staticmethod
     def sit():
         global counter_a
+        print("Executing sit gesture")
         leg_bend = 53
         right_back_leg = 512
         left_back_leg = 512
 
-        for _ in range (4):
+        for i in range(4):
             right_back_leg -= leg_bend
             left_back_leg += leg_bend
             leg_positions = [512, 512, 512, 512, 512, 512, 512, 512, right_back_leg, 512, 512, left_back_leg]
+            print(f"Setting leg positions: {leg_positions}")
             esp32.servos_set_position(leg_positions)
             time.sleep(0.1)
 
-        msg,counter_a = helper_movement.toggle_activation(counter_a)
+        msg, counter_a = helper_movement.toggle_activation(counter_a)
         helper_movement.pub_msg(msg, wait_time)
+        print("Sit gesture completed")
 
-    #gesture 2 give me paw: sit down first, then extend front-left leg outward
+    @staticmethod
     def paw():
         global counter_a
-        Gesture.default()  
+        print("Executing paw gesture")
+        Gesture.default()
         Gesture.sit()
 
-        #leg_positions = [512, 512, 512, 512, 512, 512, 512, 512, 300, 512, 512, 724]  #change the value that is related t o FL
-        #esp32.servos_set_position(leg_positions)
+        # Uncomment and modify if additional movement for paw gesture is needed
+        # leg_positions = [512, 512, 512, 512, 512, 512, 512, 512, 300, 512, 512, 724]  
+        # esp32.servos_set_position(leg_positions)
 
-        msg,counter_a = helper_movement.toggle_activation(counter_a)
+        msg, counter_a = helper_movement.toggle_activation(counter_a)
         helper_movement.pub_msg(msg, wait_time)
+        print("Paw gesture completed")
 
 
-
+# Call the sit gesture
 Gesture.sit()
