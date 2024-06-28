fa53e2b3aa92f9872598703f192d6b48ce4a4018
Yuxuan He <heyux@oregonstate.edu>
2024-06-27 17:49:57 -0700

    added gesture library

---
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/gesture_lib.py
@@ -0,0 +1,63 @@
+#from UDPComms import Publisher
+#import pygame
+#import netifaces as ni
+#from PIL import Image, ImageDraw, ImageFont
+#from enum import Enum
+#from MangDang.LCD.ST7789 import ST7789
+#import sounddevice as sd
+#import soundfile as sf
+import time
+#import os
+#from display import Display
+#from display import BehaviorState
+from mini_pupper_bsp.Python_Module.MangDang.mini_pupper.old_files.helper_movement import *
+import time
+#import RPi.GPIO as GPIO
+from MangDang.mini_pupper.ESP32Interface import ESP32Interface
+
+esp32 = ESP32Interface()
+wait_time = 500
+#MESSAGE_RATE = 20
+counter_a = 0
+
+class Gesture:
+
+    #gesture 1 stand
+    def default():
+        #starting leg positions
+        initial_position = [512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512]  
+        esp32.servos_set_position(initial_position)
+
+        msg,counter_a = toggle_activation(counter_a)
+        pub_msg(msg, wait_time)
+
+    #gesture 2 sit
+    def sit():
+        leg_bend = 53
+        right_back_leg = 512
+        left_back_leg = 512
+
+        for _ in range (4):
+            right_back_leg -= leg_bend
+            left_back_leg += leg_bend
+            leg_positions = [512, 512, 512, 512, 512, 512, 512, 512, right_back_leg, 512, 512, left_back_leg]
+            esp32.servos_set_position(leg_positions)
+            time.sleep(0.1)
+
+        msg,counter_a = toggle_activation(counter_a)
+        pub_msg(msg, wait_time)
+
+    #gesture 2 give me paw: sit down first, then extend front-left leg outward
+    def paw():
+        Gesture.default()  
+        Gesture.sit()
+
+        #leg_positions = [512, 512, 512, 512, 512, 512, 512, 512, 300, 512, 512, 724]  #change the value that is related t o FL
+        #esp32.servos_set_position(leg_positions)
+
+        msg,counter_a = toggle_activation(counter_a)
+        pub_msg(msg, wait_time)
+
+
+
+    
