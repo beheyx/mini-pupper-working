a65eeb5be0cfd6c969a846a84dbda3ff9d803e0f
Yuxuan He <heyux@oregonstate.edu>
2024-07-17 13:02:01 -0700

    .

---
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/play.py
@@ -0,0 +1,25 @@
+from MangDang.mini_pupper.HardwareInterface import HardwareInterface
+from MangDang.mini_pupper.Config import Configuration
+from MangDang.mini_pupper.display import Display
+from MangDang.mini_pupper.MovementScheme import MovementScheme
+from MangDang.mini_pupper.MovementGroup import MovementGroups
+
+
+Move = MovementGroups()
+
+Move.look_right()
+Move.look_upperright()
+Move.look_up()
+Move.look_upperleft()
+Move.look_left()
+Move.look_leftlower()
+Move.look_down()
+Move.look_rightlower()
+Move.look_right()
+Move.stop()
+Move.move_right()
+Move.move_forward()
+Move.move_left()
+Move.move_backward()
+Move.move_right()
+Move.stop()
