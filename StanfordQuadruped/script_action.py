e286a1be04dfb7851ec188f9efb58f71ed1cd56e
Yuxuan He <heyux@oregonstate.edu>
2024-07-18 13:17:11 -0700

    try

---
+++ StanfordQuadruped/script_action.py
@@ -0,0 +1,13 @@
+from src.MovementGroup import MovementGroups
+
+Move = MovementGroups()
+
+#start, introducing pupper
+#pupper says hello, dance, etc.
+Move.head_ellipse()
+
+
+
+
+
+MovementLib = Move.MovementLib
