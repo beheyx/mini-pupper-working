9a684baff9d79dac86baaf52efdce37df3358e4c
Yuxuan He <heyux@oregonstate.edu>
2024-07-22 16:28:57 -0700

    try

--- StanfordQuadruped/script_action.py
+++ StanfordQuadruped/script_action.py
@@ -4,51 +4,43 @@ Move = MovementGroups()
 
 #start, introducing pupper
 #pupper says hello, dance, etc.
-# Move.head_ellipse()
-# Move.look_up()
-# Move.look_down()
-# Move.stop()
+Move.head_ellipse()
+Move.look_up()
+Move.look_down()
+Move.stop()
 
-import RPi.GPIO as GPIO
-
-def touch_init():
-    # There are 4 areas for touch actions
-    # Each GPIO to each touch area
-    touchPin_Front = 6
-    touchPin_Left  = 3
-    touchPin_Right = 16
-    touchPin_Back  = 2
-
-    # Use GPIO number but not PIN number
-    GPIO.setmode(GPIO.BCM)
-
-    # Set up GPIO numbers to input
-    GPIO.setup(touchPin_Front, GPIO.IN)
-    GPIO.setup(touchPin_Left,  GPIO.IN)
-    GPIO.setup(touchPin_Right, GPIO.IN)
-    GPIO.setup(touchPin_Back,  GPIO.IN)
+start_pupper = True
 
 
-if __name__ == "__main__":
+list_of_commands = {
+    #basic movement: wasd keys
+    "w": Move.move_forward,
+    "s": Move.move_backward,
+    "a": Move.rotate(10),   #turn left 10 degree
+    "d": Move.rotate(-10),  #turn right 10 degree
 
+    #look movements: string input
+    "look up": Move.look_up(),
+    "look down": Move.look_down(),
+    "look right": Move.look_right(),
+    "look left": Move.look_left()
+}
 
-    touchPin_Front = 6
-    touchPin_Left  = 3
-    touchPin_Right = 16
-    touchPin_Back  = 2
+while start_pupper:
+    action = input("Next move (type 'exit' to quit): ")
 
-    # Use GPIO number but not PIN number
-    GPIO.setmode(GPIO.BCM)
+    if action == "exit":
+        start_pupper = False
+        print("Pupper stopped.")
+    
+    elif action in list_of_commands:
+        print(f"Executing action: {action}")
+        calling_action = list_of_commands[action]
+        calling_action() #calls the corr action
 
-    # Set up GPIO numbers to input
-    GPIO.setup(touchPin_Front, GPIO.IN)
-    GPIO.setup(touchPin_Left,  GPIO.IN)
-    GPIO.setup(touchPin_Right, GPIO.IN)
-    GPIO.setup(touchPin_Back,  GPIO.IN)
-
-
-    while True:
-        
+    else:
+        Move.body_row(30)
+        print("Invalid action, pupper is confused")
 
 
 MovementLib = Move.MovementLib
