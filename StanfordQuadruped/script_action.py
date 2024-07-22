79a109c56835b4bb59f5d27e61bf0f35cf4c5d4a
Yuxuan He <heyux@oregonstate.edu>
2024-07-22 16:36:34 -0700

    .

--- StanfordQuadruped/script_action.py
+++ StanfordQuadruped/script_action.py
@@ -12,13 +12,13 @@ Move.head_ellipse()
 start_pupper = True
 
 list_of_commands = {
-    # Basic movement: wasd keys
+    #asic movement: wasd keys
     "w": Move.move_forward,
     "s": Move.move_backward,
-    "a": lambda: Move.rotate(10),   # Turn left 10 degrees
-    "d": lambda: Move.rotate(-10),  # Turn right 10 degrees
+    "a": lambda: Move.rotate(10),   #turn left 10 degrees
+    "d": lambda: Move.rotate(-10),  #turn right 10 degrees
 
-    # Look movements: string input
+    #look movements: string input
     "look up": Move.look_up,
     "look down": Move.look_down,
     "look right": Move.look_right,
@@ -35,8 +35,9 @@ while start_pupper:
     elif action in list_of_commands:
         print(f"Executing action: {action}")
         calling_action = list_of_commands[action]
-        calling_action()  # Calls the correct action
+        calling_action()  #calls the correct action
 
     else:
         Move.body_row(30)
         print("Invalid action, pupper is confused")
+
