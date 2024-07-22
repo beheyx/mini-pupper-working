94b4766080fed90473a079b8d91dfcc5358cd3ae
Yuxuan He <heyux@oregonstate.edu>
2024-07-22 16:35:44 -0700

    ..

--- StanfordQuadruped/script_action.py
+++ StanfordQuadruped/script_action.py
@@ -2,45 +2,41 @@ from src.MovementGroup import MovementGroups
 
 Move = MovementGroups()
 
-#start, introducing pupper
-#pupper says hello, dance, etc.
+# Start, introducing pupper
+# Pupper says hello, dance, etc.
 Move.head_ellipse()
-#Move.look_up()
-#Move.look_down()
-#Move.stop()
+# Move.look_up()
+# Move.look_down()
+# Move.stop()
 
-# start_pupper = True
+start_pupper = True
 
+list_of_commands = {
+    # Basic movement: wasd keys
+    "w": Move.move_forward,
+    "s": Move.move_backward,
+    "a": lambda: Move.rotate(10),   # Turn left 10 degrees
+    "d": lambda: Move.rotate(-10),  # Turn right 10 degrees
 
-# list_of_commands = {
-#     #basic movement: wasd keys
-#     "w": Move.move_forward,
-#     "s": Move.move_backward,
-#     "a": Move.rotate(10),   #turn left 10 degree
-#     "d": Move.rotate(-10),  #turn right 10 degree
+    # Look movements: string input
+    "look up": Move.look_up,
+    "look down": Move.look_down,
+    "look right": Move.look_right,
+    "look left": Move.look_left
+}
 
-#     #look movements: string input
-#     "look up": Move.look_up(),
-#     "look down": Move.look_down(),
-#     "look right": Move.look_right(),
-#     "look left": Move.look_left()
-# }
+while start_pupper:
+    action = input("Next move (type 'exit' to quit): ")
 
-# while start_pupper:
-#     action = input("Next move (type 'exit' to quit): ")
-
-#     if action == "exit":
-#         start_pupper = False
-#         print("Pupper stopped.")
+    if action == "exit":
+        start_pupper = False
+        print("Pupper stopped.")
     
-#     elif action in list_of_commands:
-#         print(f"Executing action: {action}")
-#         calling_action = list_of_commands[action]
-#         calling_action() #calls the corr action
+    elif action in list_of_commands:
+        print(f"Executing action: {action}")
+        calling_action = list_of_commands[action]
+        calling_action()  # Calls the correct action
 
-#     else:
-#         Move.body_row(30)
-#         print("Invalid action, pupper is confused")
-
-
-MovementLib = Move.MovementLib
+    else:
+        Move.body_row(30)
+        print("Invalid action, pupper is confused")
