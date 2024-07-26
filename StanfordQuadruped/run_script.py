e7a030376f1b852105351550948f51bd766b093d
Yuxuan He <heyux@oregonstate.edu>
2024-07-26 15:26:56 -0700

    ..

--- StanfordQuadruped/run_script.py
+++ StanfordQuadruped/run_script.py
@@ -1,6 +1,5 @@
 import numpy as np
 import time
-import threading
 from src.IMU import IMU
 from src.Controller import Controller
 from src.State import State
@@ -8,17 +7,13 @@ from MangDang.mini_pupper.HardwareInterface import HardwareInterface
 from MangDang.mini_pupper.Config import Configuration
 from pupper.Kinematics import four_legs_inverse_kinematics
 from MangDang.mini_pupper.display import Display
+from src.MovementScheme import MovementScheme
+from script_action import MovementLib
 from src.Command import Command
-from src.MovementGroup import MovementGroups
-
-def get_user_input():
-    while True:
-        command = input("Enter command: ")
-        if command in command_mapping:
-            command_queue.append(command)
 
 def main(use_imu=False):
-    """Main program"""
+    """Main program
+    """
 
     # Create config
     config = Configuration()
@@ -38,17 +33,15 @@ def main(use_imu=False):
     )
     state = State()
 
+    #Create movement group scheme instance and set a default True state
+    movementCtl = MovementScheme(MovementLib)
+    dance_active_state = True
+    lib_length = len(MovementLib)
+
     last_loop = time.time()
 
     command = Command()
-
-    # Create an instance of MovementGroups
-    movement_groups = MovementGroups()
-
-    # Start a separate thread to get user input
-    input_thread = threading.Thread(target=get_user_input)
-    input_thread.daemon = True
-    input_thread.start()
+    command.pseudo_dance_event = True
 
     while True:
         now = time.time()
@@ -62,39 +55,25 @@ def main(use_imu=False):
         )
         state.quat_orientation = quat_orientation
 
-        # Check if there is a command in the queue
-        if command_queue:
-            user_command = command_queue.pop(0)
-            if user_command == "exit":
-                print("Pupper stopped")
-                break
-            else:
-                command_mapping[user_command]()
-                print(f"Executing action: {user_command}")
-
         # Step the controller forward by dt
-        if movement_groups.MovementLib:
-            movement = movement_groups.MovementLib.pop(0)
-            state.joint_angles = controller.run(state, movement, disp)
+        if dance_active_state == True:
+            # Caculate legsLocation, attitudes and speed using custom movement script
+            movementCtl.runMovementScheme()
+            command.legslocation        = movementCtl.getMovemenLegsLocation()
+            command.horizontal_velocity = movementCtl.getMovemenSpeed()
+            command.roll                = movementCtl.attitude_now[0]
+            command.pitch               = movementCtl.attitude_now[1]
+            command.yaw                 = movementCtl.attitude_now[2]
+            command.yaw_rate            = movementCtl.getMovemenTurn()
+            controller.run(state, command, disp)
+        else:
+            controller.run(state, command, disp)
+        if movementCtl.movement_now_number >= lib_length - 1 and movementCtl.tick >= movementCtl.now_ticks:
+            print("exit the process")
+            break
+
 
         # Update the pwm widths going to the servos
         hardware_interface.set_actuator_postions(state.joint_angles)
-
-# Command queue and mapping
-command_queue = []
-
-movement_groups = MovementGroups()
-
-command_mapping = {
-    "w": movement_groups.move_forward(),
-    "s": movement_groups.move_backward(),
-    "a": lambda: movement_groups.rotate(10),
-    "d": lambda: movement_groups.rotate(-10),
-    "look up": movement_groups.look_up(),
-    "look down": movement_groups.look_down(),
-    "look right": movement_groups.look_right(),
-    "look left": movement_groups.look_left(),
-    "exit": movement_groups.stop()
-}
 
 main()
